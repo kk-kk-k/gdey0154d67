1b54bcb1ea1d64ac99d31dab638bc4a5aef75516
Jakub Kral <jakub6kral@centrum.cz>
2026-02-07 18:48:06 +0100

    Added license specifiers

--- gdey0154d67.c
+++ gdey0154d67.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
 /**
  * @file gdey0154d67.c
  * @author Jakub Kral (jakub6kral@centrum.cz)
@@ -5,6 +6,20 @@
  * @details Driver functionality is described in gdey0154d67_private.h
  * @version 0.1
  * @date 2026-02-01
+ * @copyright 2026 Jakub Kral (jakub6kral@centrum.cz)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  * 
  */
 
